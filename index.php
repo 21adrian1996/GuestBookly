2e51eafd3ddd9514ec51a02e1752f11acff34f86
Adrian <adrian@adrianberger.ch>
2015-10-20 22:03:49 +0200

    add global structur

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @author Adian Berger <adrian.berger2112@gmail.com>
+* @copyright Adian Berger <adrian.berger2112@gmail.com>
+* @link https://bitbucket.org/SuperSuperAdrian/simplybook
+* @package guestbook
+* @return emtpy
+* @since 0.0.1
+* @version 0.0.1
+**/
+
+reguire_once('Controller/controller.class.php');
+
+new Controller();
